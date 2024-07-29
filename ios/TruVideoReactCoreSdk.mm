c6268370625743a9c87fc191558bec6a5fba0ba1
moinuddin <moinuddin@cellulant.io>
2024-07-29 19:34:56 +0530

    Ios core module integration

--- ios/TruVideoReactCoreSdk.mm
+++ ios/TruVideoReactCoreSdk.mm
@@ -2,9 +2,12 @@
 
 @interface RCT_EXTERN_MODULE(TruVideoReactCoreSdk, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+RCT_EXTERN_METHOD(authetication:(NSString)apiKey withSecretKey:(NSString)secretKey
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(clearAuthentication:(RCTPromiseResolveBlock)resolve
+                withRejecter:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
