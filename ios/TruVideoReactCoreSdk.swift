c1d776bde00047c5fb6d47c1f5c22eec9b97a86e
BhaveshPathe <bhaveshpathe@BhaveshPatheMacAir.local>
2025-12-16 15:07:26 +0530

    fix(ios): update isAuthenticated and Xcode project settings

--- ios/TruVideoReactCoreSdk.swift
+++ ios/TruVideoReactCoreSdk.swift
@@ -10,7 +10,7 @@ class TruVideoReactCoreSdk: NSObject {
   @objc(isAuthenticated:withRejecter:)
   func isAuthenticated(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
       do {
-        let isAuthenticated = try TruvideoSdk.isAuthenticated
+        let isAuthenticated = try TruvideoSdk.isAuthenticated()
           print("isAuthenticated", isAuthenticated)
           // Dispatch to the main thread asynchronously
               resolve(isAuthenticated)  // Resolving after a delay
